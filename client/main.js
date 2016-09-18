6b21efdd82deff66a6ea1164a405f2a2baa6261b
henry <henryyogman@gmail.com>
2016-09-18 12:09:26 +0200

    inital commit

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
+});
